b849adc2416fc0ce3e32eb0eb2bb2be17d2df47d
unknown <sososaluma@gmail.com>
2022-06-16 20:42:30 +0200

    deploy-3

--- udagram/udagram-frontend/src/environments/environment.prod.ts
+++ udagram/udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: true,
   appName: "Udagram",
-  apiHost: "http://localhost:3000/api/v0",
+  apiHost: "http://Hopethisworks-env.eba-rahjqri9.us-east-1.elasticbeanstalk.com/api/v0",
 };
 
 /*
